e859077bb8e40480a36d779c38fa4b1fa5c9eaea
nanansm <76173154+nanansm@users.noreply.github.com>
2024-10-10 19:45:08 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/diksian-reactjs/', // Ganti dengan nama repository GitHub Anda
+  base: '/my-react-app/', // Ganti dengan nama repository GitHub Anda
 });
